bc7105d65be3a24248bdc674ebcee2a66cebf71b
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-05-19 20:45:55 +0200

    use calculator

--- ToyPaketCalculatorClient/Program.cs
+++ ToyPaketCalculatorClient/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace ToyPaketCalculatorClient
+﻿using System;
+
+namespace ToyPaketCalculatorClient
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(ToyPaketCalculator.Calculator.add(3, 5));
         }
     }
 }
